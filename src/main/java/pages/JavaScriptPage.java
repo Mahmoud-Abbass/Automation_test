3e7a6b6fd32731624c43d43792e439ae87814085
Mahmoud-abbass <abbasmahmoud304@gmail.com>
2023-10-28 20:20:17 +0200

    test

--- src/main/java/pages/JavaScriptPage.java
+++ src/main/java/pages/JavaScriptPage.java
@@ -12,12 +12,21 @@ public class JavaScriptPage{
 
     private final By clickForJsAlertButton = By.xpath("//*[@id=\"content\"]/div/ul/li[1]/button");
     private final By validationMessage = By.id("result");
+    private final By jSConfirmButton = By.xpath("//*[@id=\"content\"]/div/ul/li[2]/button");
+    private final By jsPromotButton = By.xpath("//*[@id=\"content\"]/div/ul/li[3]/button");
     public void selectclickForJsAlertButton(){
         driver.findElement(clickForJsAlertButton).click();
     }
         public void acceptAlert(){
         driver.switchTo().alert().accept();
         }
+        public void ClickOnJsConfirmButton (){
+        driver.findElement(jSConfirmButton).click();
+        }
+        public void ClickOnJsPromotButton(){
+        driver.findElement(jsPromotButton).click();
+        }
+
 
     public String getValidationMessage() {
         return driver.findElement(validationMessage).getText();
--- src/main/java/pages/WYSIWYGEditorPage.java
+++ src/main/java/pages/WYSIWYGEditorPage.java
@@ -9,6 +9,6 @@ public class WYSIWYGEditorPage {
         this.driver = driver;
     }
     private final By increaseIndentButton = By.xpath("//*[@id=\"content\"]/div/div/div[1]/div[1]/div[2]/div/div[5]/button[2]");
-private final By
+private final By textArea
 
 }
